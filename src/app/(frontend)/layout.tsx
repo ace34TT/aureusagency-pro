2257a7a9680af56dc012a5d9000f6ce21f2ebd1d
aceky <tafinasoa35@gmail.com>
2026-02-17 22:27:40 +0300

    Add Google Tag Manager to frontend layout
    - Integrated Google Tag Manager script with `next/script` for after-interactive loading.
    - Included `noscript` iframe fallback for users with JavaScript disabled.

--- src/app/(frontend)/layout.tsx
+++ src/app/(frontend)/layout.tsx
@@ -13,6 +13,8 @@ import { InitTheme } from '@/providers/Theme/InitTheme'
 import { mergeOpenGraph } from '@/utilities/mergeOpenGraph'
 import { draftMode } from 'next/headers'
 
+import Script from 'next/script'
+
 import './globals.css'
 import '@/styles/typography.css'
 import { getServerSideURL } from '@/utilities/getURL'
@@ -26,8 +28,25 @@ export default async function RootLayout({ children }: { children: React.ReactNo
         <InitTheme />
         <link href="/favicon.ico" rel="icon" sizes="32x32" />
         {/*<link href="/favicon.svg" rel="icon" type="image/svg+xml" />*/}
+        <Script id="google-tag-manager" strategy="afterInteractive">
+          {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-N4SN4C35');
+          `}
+        </Script>
       </head>
       <body>
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-N4SN4C35"
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          />
+        </noscript>
         <Providers>
           {/*<AdminBar*/}
           {/*  adminBarProps={{*/}
