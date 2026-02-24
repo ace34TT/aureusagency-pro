9896111a7c1e94deb6062ab187c1daafb15bfb91
aceky <tafinasoa35@gmail.com>
2026-02-24 09:39:36 +0300

    refactor: Remove unused commented code from `layout.tsx`

--- src/app/(frontend)/layout.tsx
+++ src/app/(frontend)/layout.tsx
@@ -40,35 +40,10 @@ export default async function RootLayout({ children }: { children: React.ReactNo
       <head>
         <InitTheme />
         <link href="/favicon.ico" rel="icon" sizes="32x32" />
-        {/*<link href="/favicon.svg" rel="icon" type="image/svg+xml" />*/}
-        {/*<Script id="google-tag-manager" strategy="afterInteractive">*/}
-        {/*  {`*/}
-        {/*    (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':*/}
-        {/*    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],*/}
-        {/*    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=*/}
-        {/*    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);*/}
-        {/*    })(window,document,'script','dataLayer','GTM-N4SN4C35');*/}
-        {/*  `}*/}
-        {/*</Script>*/}
       </head>
       <body>
         <GoogleTagManager gtmId="GTM-N4SN4C35" />
-
-        {/*<noscript>*/}
-        {/*  <iframe*/}
-        {/*    src="https://www.googletagmanager.com/ns.html?id=GTM-N4SN4C35"*/}
-        {/*    height="0"*/}
-        {/*    width="0"*/}
-        {/*    style={{ display: 'none', visibility: 'hidden' }}*/}
-        {/*  />*/}
-        {/*</noscript>*/}
         <Providers>
-          {/*<AdminBar*/}
-          {/*  adminBarProps={{*/}
-          {/*    preview: isEnabled,*/}
-          {/*  }}*/}
-          {/*/>*/}
-
           <Header />
           {children}
           <Footer />
